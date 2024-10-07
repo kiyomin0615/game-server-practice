6d58ec4c20cfc18934e9ca3af0b303941a74caeb
kiyomin0615 <moeday1010@gmail.com>
2024-10-07 16:13:10 +0900

    compare performance with/without cache

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -5,33 +5,37 @@ namespace ServerCore
 {
   class Program
   {
-    // 멀티 쓰레딩 환경에서 컴파일러의 코드 최적화를 막는다
-    // volatile은 사용하지 않는 것을 권장한다
-    volatile static bool stop = false;
-
-    static void MainThread()
-    {
-      Console.WriteLine("Thread Start!");
-
-      while (!stop)
-      {
-        Console.WriteLine("Thread is looping!");
-      }
-
-      Console.WriteLine("Thread End!");
-    }
-
     static void Main(string[] args)
     {
-      Console.WriteLine("Main Thread Start!");
-      Task task = new Task(MainThread);
-      task.Start();
+      int[,] arr = new int[10000, 10000];
 
-      Thread.Sleep(1000);
-      stop = true;
-      task.Wait(); // 태스크(쓰레드) 종료될 때까지 대기
+      // with cache
+      {
+        long start = DateTime.Now.Ticks;
+        for (int y = 0; y < 10000; y++)
+        {
+          for (int x = 0; x < 10000; x++)
+          {
+            arr[y, x] = 1;
+          }
+        }
+        long end = DateTime.Now.Ticks;
+        Console.WriteLine($"It took {end - start}"); // 2769700
+      }
 
-      Console.WriteLine("Main Thread End!");
+      // without cache
+      {
+        long start = DateTime.Now.Ticks;
+        for (int y = 0; y < 10000; y++)
+        {
+          for (int x = 0; x < 10000; x++)
+          {
+            arr[x, y] = 1;
+          }
+        }
+        long end = DateTime.Now.Ticks;
+        Console.WriteLine($"It took {end - start}"); // 7151980
+      }
     }
   }
 }

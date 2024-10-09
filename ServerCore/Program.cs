08e7949e20c748ecb584d118fe967f742b37a5cf
kiyomin0615 <moeday1010@gmail.com>
2024-10-09 11:40:46 +0900

    add basic TCP server that listens for client connections, receives data, and sends a welcome message before disconnecting

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,25 +1,53 @@
 ﻿using System;
-using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
     class Program
     {
-        /*
-          TLS(Thread Local Storage)
-          쓰레드 사이에 공유되지 않는 저장 공간(스택과는 별개)
-        */
-        static ThreadLocal<string> threadName = new ThreadLocal<string>();
-
-        static void WhoAmI() {
-            threadName.Value = $"Thread ID: {Thread.CurrentThread.ManagedThreadId}";
-            Thread.Sleep(1000);
-            Console.WriteLine(threadName.Value);
-        }
-
         static void Main(string[] args)
         {
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI);
+            string hostName = Dns.GetHostName(); // 호스트 네임
+            IPHostEntry ipInstance = Dns.GetHostEntry(hostName); // ip 관련 인스턴스
+            IPAddress ip = ipInstance.AddressList[0]; // ip 주소
+            IPEndPoint endPoint = new IPEndPoint(ip, 7777); // 엔드포인트
+
+            try
+            {
+                Socket listener = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp); // TCP 프로토콜
+
+                listener.Bind(endPoint);
+
+                listener.Listen(10); // 최대 대기 가능한 클라이언트 숫자: 10
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 1. Connect
+                    Socket session = listener.Accept(); // 클라이언트의 연결 수락
+
+                    // 2. Receive
+                    byte[] receiveBuffer = new byte[1024];
+                    int size = session.Receive(receiveBuffer); // 클라이언트로부터 데이터를 전송받는다
+                    string data = Encoding.UTF8.GetString(receiveBuffer, 0, size);
+                    Console.WriteLine($"[From Client] {data}");
+
+                    // 3. Send
+                    byte[] sendBuffer = Encoding.UTF8.GetBytes("Welcome to game server!");
+                    session.Send(sendBuffer); // 클라이언트로 데이터를 전송한다
+
+                    // 4. Disconnect
+                    session.Shutdown(SocketShutdown.Both); // 클라이언트와 서버 사이의 데이터 송수신 종료
+                    session.Close(); // 클라이언트의 연결 끊기
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }

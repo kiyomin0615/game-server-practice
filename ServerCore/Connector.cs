c8857be79ab229c601e00bd7397cae7ccbee3731
kiyomin0615 <moeday1010@gmail.com>
2024-10-13 10:48:45 +0900

    add conenctor for connecting to server

---
+++ ServerCore/Connector.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    public class Connector
+    {
+        Func<Session> sessionFactory;
+
+        public void Connect(IPEndPoint endPoint, Func<Session> sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnConnectCompleted);
+            args.RemoteEndPoint = endPoint;
+            args.UserToken = socket;
+
+            StartConnecting(args);
+        }
+
+        void StartConnecting(SocketAsyncEventArgs args)
+        {
+            Socket socket = args.UserToken as Socket;
+            if (socket == null)
+                return;
+
+            bool isPending = socket.ConnectAsync(args);
+            if (isPending)
+            {
+                OnConnectCompleted(null, args);
+            }
+        }
+
+        void OnConnectCompleted(object? sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                Session session = sessionFactory.Invoke();
+                session.Start(args.ConnectSocket);
+                session.OnConnected(args.RemoteEndPoint);
+            }
+            else
+            {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+        }
+    }
+}
